36b20b8077043603380dc915e210a19852028eff
Ehsan Akhgari <ehsan@mozilla.com>
2015-03-26 09:33:13 -0400

    printf debugging

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 onfetch = function(e) {
+  dump("got " + e.request.url + "\n");
   if (e.request.url.indexOf("beaconEndpoint") >= 0) {
     e.respondWith(
       clients.matchAll({includeUncontrolled:true})
         .then(function(clients) {
+          dump("found " + client.length + " clients\n");
           clients.forEach(function(client) {
             client.postMessage({data: "beacon"});
+            dump("message posted\n");
           });
           // Note: A real beacon would probably let the request go to the network.
           return new Response("ack");
         })
     );
+    dump("respondWith\n");
   }
 };
