dbc105cd296b6ce00f265c7ff7dba98df496f777
Ehsan Akhgari <ehsan@mozilla.com>
2015-03-26 10:02:06 -0400

    Remove the debugging printfs

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
 onfetch = function(e) {
-  dump("got " + e.request.url + "\n");
   if (e.request.url.indexOf("beaconEndpoint") >= 0) {
     e.respondWith(
       clients.matchAll()
         .then(function(clients) {
-          dump("found " + clients.length + " clients\n");
           clients.forEach(function(client) {
             client.postMessage({data: "beacon"});
-            dump("message posted\n");
           });
           // Note: A real beacon would probably let the request go to the network.
           return new Response("ack");
         })
     );
-    dump("respondWith\n");
   }
 };
