417695799998d74997e8492d56b43d3dd1f5169d
Ehsan Akhgari <ehsan@mozilla.com>
2015-04-15 08:19:17 -0400

    Update the SW again

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 onfetch = function(e) {
   if (e.request.url.indexOf("check") >= 0) {
-    e.respondWith(new Response("5822ab74871a12492756519d733b7bff9fbc3575"));
+    e.respondWith(new Response("00141c6e116a2b98c8b973d9f14f35bba89b5919"));
   }
 };
