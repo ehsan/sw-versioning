ccc79b8887e7553c36b4d8d39b242682f0860d0f
Ehsan Akhgari <ehsan@mozilla.com>
2015-04-15 13:20:47 -0400

    Moar updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 onfetch = function(e) {
   if (e.request.url.indexOf("check") >= 0) {
-    e.respondWith(new Response("00141c6e116a2b98c8b973d9f14f35bba89b5919"));
+    e.respondWith(new Response("417695799998d74997e8492d56b43d3dd1f5169d"));
   }
 };
