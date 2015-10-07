d138f5721820b1bfa473e4c8dd3805e1526f47a1
Ehsan Akhgari <ehsan@mozilla.com>
2015-10-07 17:12:36 -0400

    Add a reject page

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 onfetch = function(e) {
   if (e.request.url.indexOf("check") >= 0) {
     e.respondWith(new Response("517c06a67aaa919352bf16c6aee0872f16ac7968"));
+  } else if (e.request.url.indexOf("reject") >= 0) {
+    e.respondWith(Promise.reject("oops"));
   }
 };
